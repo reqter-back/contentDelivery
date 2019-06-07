1f86b769c4225ff7e2d4e58107287526429b3968
fakhrad <fakhrad@openratio.com>
2019-06-07 08:58:33 +0430

    request save added to delivery service

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -3,20 +3,51 @@ const { sanitizeBody } = require('express-validator/filter');
 const broker = require('./serviceBroker');
 
 exports.submitRequest = function(req, res, next) {
-    broker.sendRPCMessage({clientId : req.clientId, body : req.body}, 'submitrequest').then((result)=>{
-        var obj = JSON.parse(result.toString('utf8'));
-        if (!obj.success)
+    var request = new Requests({
+        sys : {},
+        contentType : req.body.contentType,
+        category : req.body.category,
+        title : req.body.title,
+        description : req.body.description,
+        longDesc : {},
+        longDesc : req.body.longDesc,
+        thumbnail : req.body.thumbnail,
+        attachments : req.body.attachments,
+        receiver : req.body.receiver,
+        status : "draft",
+        statusLog : [],
+        settings : req.body.settings
+    });
+
+    var newStatus = {}
+    newStatus.code = "draft";
+    newStatus.applyDate = new Date();
+    newStatus.user = req.userId;
+    newStatus.description = "Item created";
+    request.status = "draft";
+    request.statusLog.push(newStatus);
+
+    request.sys.type = "request";
+    request.sys.link = uniqid();
+    request.sys.spaceId = req.spaceid;
+    request.sys.issuer = req.userId;
+    request.sys.issueDate = new Date();
+    request.sys.spaceId = req.spaceId;
+    request.save(function(err){
+        var result = {success : false, data : null, error : null };
+        if (err)
         {
-            if (obj.error)
-                return res.status(500).json(obj);
-            else
-            {
-                res.status(404).json(obj);
-            }
+            result.success = false;
+            result.data =  undefined;
+            result.error = err;
+            cb(result);       
+            return; 
         }
-        else
-        {
-            res.status(200).json(obj.data);
-        }
+        //Successfull. 
+        //Publish user registered event
+        result.success = true;
+        result.error = undefined;
+        result.data =  request;
+        cb(result); 
     });
 }
