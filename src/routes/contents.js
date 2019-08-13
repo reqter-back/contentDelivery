021c7cd5fe4bc047f18ed1de470bf6140fd25888
fakhrad <fakhrad@openratio.com>
2019-08-13 11:47:23 +0430

    some updates

--- src/routes/contents.js
+++ src/routes/contents.js
@@ -3,4 +3,5 @@ var router = express.Router();
 var controller = require("../controllers/contentController");
 var auth = require("../controllers/auth");
 router.get("/search", auth.verifyToken, controller.filter);
+router.get("/template/:template", auth.verifyToken, controller.loadByTemplate);
 module.exports = router;
