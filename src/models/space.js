d8aae4592f6eddd2f3a61efeab27eac1b904504b
fakhrad <fakhrad@openratio.com>
2019-09-04 12:21:02 +0430

    soace model updated

--- src/models/space.js
+++ src/models/space.js
@@ -1,23 +1,23 @@
-
 /**
  * Module dependencies.
  */
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 /**
  * Schema definitions.
  */
 
- const space = new Schema({
-  name : {type : String, required : true, max : 150, min : 3},
-  description : {type : String, max : 256},
-  image : {type : Object},
-  type : {type : String},
-  owner : {type: Schema.Types.ObjectId, ref: 'Systemuser' , required : true},
-  roles : [],
-  locales : [],
-  webhooks : []
+const space = new Schema({
+  name: { type: String, required: true, max: 150, min: 3 },
+  description: { type: String, max: 256 },
+  image: { type: Object },
+  type: { type: String },
+  notification_email: { type: String },
+  owner: { type: Schema.Types.ObjectId, ref: "Systemuser", required: true },
+  roles: [],
+  locales: [],
+  webhooks: []
 });
 
-module.exports = mongoose.model('Space', space);
+module.exports = mongoose.model("Space", space);
