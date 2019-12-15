ee190561633e540171559e04b2cfd7a3c6f6f951
fakhrad <fakhrad@openratio.com>
2019-12-15 12:42:00 +0330

    some testing purpose updates added

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -116,7 +116,198 @@ exports.filter = function(req, res, next) {
       });
     });
 };
+exports.test = function(req, res, next) {
+  res.send({ hello: "world" });
+};
+exports.querytest = function(req) {
+  console.log(req.query);
+  var loadrelations = req.query.loadrelations == "false" ? false : true;
+  delete req.query.loadrelations;
+  var skip = parseInt(req.query.skip) || 0;
+  delete req.query.skip;
+  var limit = parseInt(req.query.limit) || 100;
+  delete req.query.limit;
+  var sort = req.query.sort || "-sys.issueDate";
+  delete req.query.sort;
+  Contents.find(req.query)
+    .select("fields sys.issuer, sys.issueDate _id, status")
+    .populate("contentType", "_id title")
+    .skip(skip)
+    .limit(limit)
+    .sort(sort)
+    .exec((err, cts) => {
+      if (err) {
+        console.log({ success: false, error: err });
+        return;
+      }
+      if (loadrelations) {
+        try {
+          var ctypes = [];
+          for (var i = 0; i < cts.length; i++) {
+            var ct = cts[i].contentType._id.toString();
+            if (ctypes.indexOf(ct) == -1) ctypes.push(ct);
+          }
+          var relfieldarr = {};
+          var ids = [];
+          ContentTypes.find({ _id: { $in: ctypes } }).exec((err, ttypes) => {
+            if (err) {
+              console.log(cts);
+              return;
+            } else {
+              ttypes.forEach(ctype => {
+                var relfields = [];
+                for (var field in ctype.fields) {
+                  if (ctype.fields[field].type === "reference") {
+                    var references = ctype.fields[field].references;
+                    if (references) {
+                      references.forEach(ref => {
+                        relfields.push({
+                          name: ctype.fields[field].name,
+                          ctype: ref,
+                          select: ctype.fields[field].fields
+                        });
+                      });
+                    } else {
+                      relfields.push({
+                        name: ctype.fields[field].name,
+                        ctype: undefined,
+                        select: undefined
+                      });
+                    }
+                  }
+                }
+                relfieldarr[ctype._id.toString()] = relfields;
+                for (var i = 0; i < cts.length; i++) {
+                  content = cts[i];
+                  if (
+                    content.contentType._id.toString() == ctype._id.toString()
+                  ) {
+                    if (relfields.length > 0) {
+                      relfields.forEach(fld => {
+                        if (
+                          content.fields[fld.name] &&
+                          content.fields[fld.name].length > 0
+                        ) {
+                          if (isArray(content.fields[fld.name])) {
+                            content.fields[fld.name].forEach(item => {
+                              if (
+                                item.length > 0 &&
+                                ids.indexOf(item) == -1 &&
+                                mongoose.Types.ObjectId.isValid(item)
+                              )
+                                ids.push(item);
+                            });
+                          } else {
+                            if (
+                              mongoose.Types.ObjectId.isValid(
+                                content.fields[fld.name]
+                              )
+                            ) {
+                              if (ids.indexOf(content.fields[fld.name]) == -1)
+                                ids.push(content.fields[fld.name]);
+                            }
+                          }
+                        }
+                      });
+                    }
+                  }
+                }
+              });
+              Contents.find({
+                _id: { $in: ids }
+              })
+                .select("fields _id contentType")
+                .exec((err, rels) => {
+                  if (err) {
+                    console.log(cts);
+                    return;
+                  }
+                  for (var l = 0; l < ttypes.length; l++) {
+                    var ctype = ttypes[l];
+                    relfields = relfieldarr[ctype._id.toString()];
+                    console.log(relfields);
+                    relfields.forEach(fld => {
+                      for (var s = 0; s < cts.length; s++) {
+                        content = cts[s];
+                        if (
+                          content.contentType._id.toString() ==
+                          ctype._id.toString()
+                        ) {
+                          if (
+                            content.fields[fld.name] &&
+                            content.fields[fld.name].length > 0
+                          ) {
+                            if (isArray(content.fields[fld.name])) {
+                              for (
+                                i = 0;
+                                i < content.fields[fld.name].length;
+                                i++
+                              ) {
+                                var item = content.fields[fld.name][i];
+                                var row = rels.filter(
+                                  a => a._id.toString() === item.toString()
+                                );
+                                if (row.length > 0) {
+                                  var rw = {};
+                                  rw._id = row[0]._id;
+                                  rw.fields = {};
+                                  if (fld.select && fld.select.length > 0) {
+                                    for (
+                                      var j = 0;
+                                      j < fld.select.length;
+                                      j++
+                                    ) {
+                                      f = fld.select[j];
+                                      var c = row[0].fields[f];
+                                      rw.fields[f] = c;
+                                    }
+                                    content.fields[fld.name][i] = rw;
+                                  } else {
+                                    content.fields[fld.name][i] = row[0];
+                                  }
+                                }
+                              }
+                            } else {
+                              var row = rels.filter(
+                                a =>
+                                  a._id.toString() ===
+                                  content.fields[fld.name].toString()
+                              );
+                              if (row.length > 0) {
+                                var rw = {};
+                                rw.fields = {};
 
+                                rw._id = row[0]._id;
+                                if (fld.select && fld.select.length > 0) {
+                                  for (var j = 0; j < fld.select.length; j++) {
+                                    f = fld.select[j];
+                                    rw.fields[f] = row[0].fields[f];
+                                  }
+                                  console.log(rw);
+                                  content.fields[fld.name] = rw;
+                                } else {
+                                  content.fields[fld.name] = row[0];
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    });
+                  }
+                  console.log(cts);
+                });
+            }
+          });
+        } catch (e) {
+          console.log(e);
+          console.log(cts);
+        }
+      } else {
+        console.log(cts);
+      }
+    });
+};
 exports.query = function(req, res, next) {
   console.log(req.query);
   var loadrelations = req.query.loadrelations == "false" ? false : true;
--- src/routes/contents.js
+++ src/routes/contents.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var controller = require("../controllers/contentController");
 var auth = require("../controllers/auth");
+router.get("/test", controller.test);
 router.get("/search", auth.verifyToken, controller.filter);
 router.get("/query", auth.verifyToken, controller.query);
 router.get("/template/:template", auth.verifyToken, controller.loadByTemplate);
